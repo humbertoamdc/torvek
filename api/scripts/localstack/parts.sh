bf8f38fd89821bebc3b80b07f31d77cfefb44b12
Humberto <20918765+humbertomdc@users.noreply.github.com>
2024-01-14 11:47:17 -0600

    Parts infra (#19)
    * parts infra

---
+++ api/scripts/localstack/parts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# DynamoDB Tables
+awslocal dynamodb create-table \
+    --table-name Parts \
+    --attribute-definitions \
+        AttributeName=client_id#project_id#quotation_id,AttributeType=S \
+        AttributeName=id,AttributeType=S \
+    --key-schema \
+        AttributeName=client_id#project_id#quotation_id,KeyType=HASH \
+        AttributeName=id,KeyType=RANGE \
+    --billing-mod PAY_PER_REQUEST \
