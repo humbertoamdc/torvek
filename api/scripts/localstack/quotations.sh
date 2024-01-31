af8c06bfbdb6a335f42812f5414b1756385c21f7
Humberto Atondo <humberto_atondo@hotmail.com>
2024-01-30 22:23:49 -0700

    infra and localstack changes

--- api/scripts/localstack/quotations.sh
+++ api/scripts/localstack/quotations.sh
@@ -6,7 +6,22 @@ awslocal dynamodb create-table \
     --attribute-definitions \
         AttributeName=client_id#project_id,AttributeType=S \
         AttributeName=id,AttributeType=S \
+        AttributeName=status,AttributeType=S \
+        AttributeName=created_at,AttributeType=S \
     --key-schema \
         AttributeName=client_id#project_id,KeyType=HASH \
         AttributeName=id,KeyType=RANGE \
     --billing-mod PAY_PER_REQUEST \
+    --global-secondary-indexes \
+    '[
+      {
+        "IndexName": "QuotationsByStatus",
+        "KeySchema": [
+          {"AttributeName":"status","KeyType":"HASH"},
+          {"AttributeName":"created_at","KeyType":"RANGE"}
+        ],
+        "Projection":{
+          "ProjectionType":"ALL"
+        }
+      }
+    ]'
