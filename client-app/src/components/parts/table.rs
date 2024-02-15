c076e106994a2417699ae5c0e27a481d5259171b
Humberto Atondo <humberto_atondo@hotmail.com>
2024-02-14 21:56:05 -0700

    update parts table colors

--- client-app/src/components/parts/table.rs
+++ client-app/src/components/parts/table.rs
@@ -1,13 +1,15 @@
+use leptos::*;
+
+use api_boundary::parts::models::Part;
+
 use crate::components::parts::table_row::PartsTableRow;
 use crate::models::reactive_part::ReactivePart;
-use api_boundary::parts::models::Part;
-use leptos::*;
 
 #[component]
 pub fn PartsTable(#[prop(into)] parts: RwSignal<Vec<Part>>) -> impl IntoView {
     view! {
-        <div class="inline-block min-w-full shadow rounded-lg overflow-hidden">
-            <table class="min-w-full leading-normal">
+        <div class="inline-block w-full shadow rounded-lg overflow-hidden">
+            <table class="w-full">
                 <thead>
                     <tr>
                         <For
@@ -28,7 +30,7 @@ pub fn PartsTable(#[prop(into)] parts: RwSignal<Vec<Part>>) -> impl IntoView {
                             key=|(_, column_name)| column_name.to_string()
                             children=move |(_, column_name)| {
                                 view! {
-                                    <th class="px-2 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                    <th class="px-2 py-3 border-b-1 border-gray-600 bg-gray-300 text-left text-xs font-semibold text-gray-800 uppercase tracking-wider">
                                         <div class="flex justify-center">{column_name}</div>
                                     </th>
                                 }
