c14a1b35cc3300ecf341b971fa23a10a1266921c
Humberto Atondo <humberto_atondo@hotmail.com>
2024-11-17 11:39:50 -0700

    add Dynamodb:BatchGetItem permission to api lambda role

--- api/infra/stripe/staging/webhooks.tf
+++ api/infra/stripe/staging/webhooks.tf
@@ -12,8 +12,8 @@ provider "stripe" {
 }
 
 resource "stripe_webhook_endpoint" "complete_checkout_session_webhook_staging" {
-  url            = "https://api.torvek.com/api/v1/payments/webhooks/complete_checkout_session"
-  description    = "Webhook used by Stripe to confirm when a quotation payment is successful."
+  url         = "https://api.torvek-staging.com/api/v1/payments/webhooks/complete_checkout_session"
+  description = "Webhook used by Stripe to confirm when a quotation payment is successful."
   enabled_events = [
     "checkout.session.completed"
   ]
